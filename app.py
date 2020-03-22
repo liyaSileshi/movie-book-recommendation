4b426c7d3cfd8ad88fc0497670a834c17ce35920
liyaSileshi <liyasileshi@gmail.com>
2020-03-21 17:50:58 -0700

    changes in app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from movies import get_five_similar_movies
 from flask import Flask, request, render_template
 from books import get_five_similar_books
+import os
 
 app = Flask(__name__)
 
@@ -26,5 +27,5 @@ def invalid_route(e):
     return "404 Page not found"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
 
