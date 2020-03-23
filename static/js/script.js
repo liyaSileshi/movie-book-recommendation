d6cb80fb951c8e7c3bdaba6872e416f275d630ed
liyaSileshi <liyasileshi@gmail.com>
2020-03-23 00:08:21 -0700

    local storage working

--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,27 @@
+const movie = document.getElementById('movie');
+const book = document.getElementById('book');
+const movieForm = document.getElementById('movieForm');
+const bookForm = document.getElementById('bookForm');
+const addWatchList = document.getElementById('addWatchList');
 
-const movie = document.getElementById('movie')
-const book = document.getElementById('book')
-const movieForm = document.getElementById('movieForm')
-const bookForm = document.getElementById('bookForm')
-let all_movies = Array.from(document.querySelectorAll('p.recmovie'))
-
-console.log(all_movies)
+let all_movies = Array.from(document.querySelectorAll('p.recmovie'));
+let fav_movies = [];
+let userdata;
+getUserData();
 
 movie.addEventListener('change', validateChoice)
 book.addEventListener('change', validateChoice)
+addWatchList.addEventListener('click', addLocal)
 
+all_movies.forEach(movie => movie.addEventListener('click', () => {
+    console.log(movie.innerHTML) //the value of the p tag
+    movie.style.opacity = 0.2;  //validates that it's selected
+    if(fav_movies.indexOf(movie.innerHTML) === -1){
+        //if item does not exist in the array
+        fav_movies.push(movie.innerHTML)
+    }
+    console.log(fav_movies)
+})) 
 
 function validateChoice() {
     if(document.getElementById('movie').checked == true){
@@ -25,4 +37,31 @@ function validateChoice() {
     }   
 }
 
+function getUserData() {
+    if (localStorage.getItem("movies")) {
+      userdata = JSON.parse(localStorage.getItem("movies"))
+    } else {
+      userdata = {
+        fav_movies : [] //empty out the movies array
+      }
+    }
+  }
 
+
+  //adds it to local storage
+function addLocal(){ 
+    fav_movies.forEach(movie => {
+        console.log('works')
+        if (userdata.fav_movies.includes(movie)){
+        console.log("don't add")
+        }
+        else {
+        console.log('add')
+        console.log(movie)
+        userdata.fav_movies.push(movie)
+        const json = JSON.stringify(userdata)
+        // Save to localStorage
+        localStorage.setItem("movies", json)
+        }
+    })
+}
